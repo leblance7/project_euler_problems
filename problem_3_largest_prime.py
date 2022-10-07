b03a3800c363a834a36cb264a1163e01bce5373a
Ethan LeBlanc <leblance7@gamil.com>
2022-10-07 12:42:30 -0500

    Create problem_3_largest_prime.py

---
+++ problem_3_largest_prime.py
@@ -0,0 +1,9 @@
+#function takes a number and returns true or false determining if prime
+def prime_number():
+    if True print("prime"):
+    else False print("not prime")
+
+for number in [1, 13195]:
+    
+     
+     
