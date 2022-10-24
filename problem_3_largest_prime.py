e505bd4080d41905cdf124369a1ff1c5cfc9898e
Ethan LeBlanc <leblance7@gamil.com>
2022-10-24 17:08:37 -0500

    Update problem_3_largest_prime.py

--- problem_3_largest_prime.py
+++ problem_3_largest_prime.py
@@ -1,9 +1,33 @@
 #function takes a number and returns true or false determining if prime
-def prime_number():
-    if True print("prime"):
-    else False print("not prime")
+def is_factor(n: int, x: int) -> bool:
+    return n % x == 0
 
-for number in [1, 13195]:
+
+def factors(n: int) -> list[int]:
+    results = []
+    for x in range(2, n):
+        if is_factor(n,x):
+            results.append(x)
+    return results
+
+
+def is_prime(n: int) -> bool:
+    if factors(n) == []:
+        return True
+    else:
+        return False
+
+        
+def print_factors(n: int) -> None:
+    print(n, "->", factors(n))
+
+
+
+print(is_prime(13))
+
+
+
+
+
     
-     
-     
+    
